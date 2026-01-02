f6a2d51b74a2a569e44f5a42f943ce68f1d26ca4
AndreaSegalla <andreasegallalav@gmail.com>
2026-01-02 19:47:20 +0100

    first commit

---
+++ build/Desktop_Qt_6_10_1-Debug/.qt/appProva_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appProva")
+set(working_dir "/home/andrea/QTProjects/Prova")
+set(src_and_dest_list
+
+)
+set(timestamp_file "/home/andrea/QTProjects/Prova/build/Desktop_Qt_6_10_1-Debug/.qt/appProva_res.txt")
