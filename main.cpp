870f556e59d0c4df5f25cf6d657c506c49df66f5
AndreaSegalla <andreasegallalav@gmail.com>
2026-01-04 11:14:05 +0100

    fixed eglfs not x11

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 
 int main(int argc, char *argv[])
 {
+    // Forza Qt a usare il plugin EGLFS (embedded, senza X11)
+    qputenv("QT_QPA_PLATFORM", QByteArray("eglfs"));
+
+    // Forza UTF-8
+    qputenv("LC_ALL", QByteArray("en_US.UTF-8"));
+    qputenv("LANG", QByteArray("en_US.UTF-8"));
+
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
